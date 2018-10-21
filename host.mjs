4cc44cd9c153dd95dab9b2c4d0bd0d5392fcdc45
Hunter Loftis <hunter@hunterloftis.com>
2018-10-20 21:37:37 -0400

    organize host update

--- host.mjs
+++ host.mjs
@@ -17,54 +17,51 @@ export default class Host {
     this.time = 0
     this.events = []
 
-    this.wss.on('connection', socket => {
-      const id = this.nextId++
-      const name = pilotName()
-      const entity = new Ship({ id, name })
-      const msg = JSON.stringify({ type: 'hello', id, name })
-
-      this.entities[id] = entity
-      socket.id = id
-      socket.inputs = []
-      socket.applied = 0
-      socket.appliedNulls = 0
-      socket.send(msg)
-      this.events.push({ msg: `${name} joined.` })
-
-      socket.on('message', str => {
-        const input = JSON.parse(str)
-        // For demo purposes only.
-        // The whole point of an authoritative server is so clients can only send input, not state.
-        if (input.type === 'hacked-state') {
-          entity.name = input.name
-        } else {
-          socket.inputs.push(input)
-        }
-      })
-
-      socket.on('close', () => {
-        socket.removeAllListeners()
-        delete this.entities[id]
-        this.events.push({ msg: `${name} left.` })
-      })
-    })
+    this.wss.on('connection', socket => this.addClient(socket))
 
     this.time = performance.now()
     setInterval(this.update.bind(this), UPDATE_INTERVAL)
   }
+  addClient(socket) {
+    const id = this.nextId++
+    const name = pilotName()
+    const entity = new Ship({ id, name })
+    const msg = JSON.stringify({ type: 'hello', id, name })
+
+    this.entities[id] = entity
+    socket.id = id
+    socket.inputs = []
+    socket.applied = 0
+    socket.appliedNulls = 0
+    socket.send(msg)
+    this.events.push({ msg: `${name} joined.` })
+
+    socket.on('message', str => {
+      const input = JSON.parse(str)
+      // For demo purposes only.
+      // The whole point of an authoritative server is so clients can only send input, not state.
+      if (input.type === 'hacked-state') {
+        entity.name = input.name
+      } else {
+        socket.inputs.push(input)
+      }
+    })
+
+    socket.on('close', () => {
+      socket.removeAllListeners()
+      delete this.entities[id]
+      this.events.push({ msg: `${name} left.` })
+    })
+  }
   update() {
     const now = performance.now()
     const sockets = Array.from(this.wss.clients.values())
-
     const simTime = now - this.time
     const ticks = Math.floor(simTime / TICK)
-
     this.time += ticks * TICK
 
     if (!sockets.length) return
 
-    // TODO: make clearer that every tick, we apply some kind of input
-    // it's either explicit (in the queue) or implied (implied null)
     for (let t = 0; t < ticks; t++) {
       if (sockets.length) {
         totalTicks++
@@ -74,58 +71,63 @@ export default class Host {
       }
       const entities = Object.values(this.entities)
       const children = []
-      entities.forEach(en => {
-        const socket = sockets.find(s => s.id === en.id)
-        const input = socket ? socket.inputs.shift() : undefined
-        if (input) {
-          totalInputs++
-          if (totalInputs % 100 === 0) {
-            console.log('total inputs:', totalInputs)
-          }
-          if (input.z && socket.appliedNulls > 0) { // ignorable input
-            socket.appliedNulls--
-          } else {  // unignorable input
-            const ch = en.simulate(TICK, input) || []
-            children.push(...ch)
-          }
-          socket.applied = input.n
-        } else {
-          const ch = en.simulate(TICK) || []
-          children.push(...ch)
-          if (socket) {
-            socket.appliedNulls++
-          }
-        }
-      })
+      entities.forEach(en => this.simulate(en, sockets, children))
       entities.forEach(en1 => {
-        entities.forEach(en2 => {
-          const ev = en1.interact(TICK, en2) || []
-          this.events.push(...ev)
-        })
+        entities.forEach(en2 => this.interact(en1, en2))
       })
-      children.forEach(child => {
-        if (child.id && this.entities[child.id]) {
-          delete this.entities[child.id]
-          return
-        }
-        child.id = this.nextId++
-        this.entities[child.id] = child
-      })
+      children.forEach(child => this.createDelete(child))
     }
 
-    this.wss.clients.forEach(socket => {
-      if (socket.readyState !== WebSocket.OPEN) return
-
-      const state = {
-        time: this.time,
-        entities: this.entities,
-        sequence: socket.applied,
-        events: this.events,
-      }
-      const msg = JSON.stringify({ type: 'state', state })
-      socket.send(msg)
-    })
+    this.wss.clients.forEach(socket => this.sendState(socket))
     this.events = []
+  }
+  simulate(en, sockets, children) {
+    const socket = sockets.find(s => s.id === en.id)
+    // TODO: clear all ignorable input immediately
+    const input = socket ? socket.inputs.shift() : undefined
+    if (input) {
+      totalInputs++
+      if (totalInputs % 100 === 0) {
+        console.log('total inputs:', totalInputs)
+      }
+      if (input.z && socket.appliedNulls > 0) { // ignorable input
+        socket.appliedNulls--
+      } else {  // unignorable input
+        const ch = en.simulate(TICK, input) || []
+        children.push(...ch)
+      }
+      socket.applied = input.n
+    } else {
+      const ch = en.simulate(TICK) || []
+      children.push(...ch)
+      if (socket) {
+        socket.appliedNulls++
+      }
+    }
+  }
+  interact(en1, en2) {
+    const ev = en1.interact(TICK, en2) || []
+    this.events.push(...ev)
+  }
+  createDelete(child) {
+    if (child.id && this.entities[child.id]) {
+      delete this.entities[child.id]
+      return
+    }
+    child.id = this.nextId++
+    this.entities[child.id] = child
+  }
+  sendState(socket) {
+    if (socket.readyState !== WebSocket.OPEN) return
+
+    const state = {
+      time: this.time,
+      entities: this.entities,
+      sequence: socket.applied,
+      events: this.events,
+    }
+    const msg = JSON.stringify({ type: 'state', state })
+    socket.send(msg)
   }
 }
 
